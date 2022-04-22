0690e4c757e21b8ff2476046a9e0de427f5d31a4
Juan David Buitrago <juan.buitrago@edunext.co>
2022-04-22 10:26:23 -0500

    Feat: Add required maintenance files

--- tutorcodejail/plugin.py
+++ tutorcodejail/plugin.py
@@ -1,12 +1,13 @@
-from glob import glob
+"""Manage the plugin for the tutorcodejail."""
+
 import os
+from glob import glob
+
 import pkg_resources
 
 from .__about__ import __version__
 
-templates = pkg_resources.resource_filename(
-    "tutorcodejail", "templates"
-)
+templates = pkg_resources.resource_filename("tutorcodejail", "templates")
 
 config = {
     "add": {
@@ -18,29 +19,28 @@ config = {
         "DOCKER_IMAGE": f"docker.io/ednxops/codejailservice:{__version__}",
         "SANDBOX_PYTHON_VERSION": "3.5.10",
     },
-    "set":{}
+    "set": {},
 }
 
 hooks = {
     "build-image": {
         "codejail": "{{ CODEJAIL_DOCKER_IMAGE }}",
-        "codejail_apparmor": f"docker.io/ednxops/codejail_apparmor:{__version__}"
+        "codejail_apparmor": f"docker.io/ednxops/codejail_apparmor:{__version__}",
     },
     "remote-image": {
         "codejail": "{{ CODEJAIL_DOCKER_IMAGE }}",
-        "codejail_apparmor": f"docker.io/ednxops/codejail_apparmor:{__version__}"
+        "codejail_apparmor": f"docker.io/ednxops/codejail_apparmor:{__version__}",
     },
-    "init": ["codejail_apparmor"]
+    "init": ["codejail_apparmor"],
 }
 
 
 def patches():
+    """Retrieve all the patches of tutorcodejail."""
     all_patches = {}
-    patches_dir = pkg_resources.resource_filename(
-        "tutorcodejail", "patches"
-    )
+    patches_dir = pkg_resources.resource_filename("tutorcodejail", "patches")
     for path in glob(os.path.join(patches_dir, "*")):
-        with open(path) as patch_file:
+        with open(path, encoding="utf-8") as patch_file:
             name = os.path.basename(path)
             content = patch_file.read()
             all_patches[name] = content
