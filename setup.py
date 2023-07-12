f7b629e697451f17299983e5fc1bf340a9e92074
henrrypg <henrry.pulgarin@edunext.co>
2023-07-12 09:29:27 -0500

    fix: pin olive tutor version due to compatibility issues with palm

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
     packages=find_packages(exclude=["tests*"]),
     include_package_data=True,
     python_requires=">=3.8",
-    install_requires=["tutor"],
+    install_requires=["tutor<16.0.0"],
     entry_points={"tutor.plugin.v1": ["codejail = tutorcodejail.plugin"]},
     classifiers=[
         "Development Status :: 3 - Alpha",
