091e3f73345820fb7d19a38225ca5c403f63500f
Maria Fernanda Magallanes Zubillaga <maria.magallanes@edunext.co>
2024-05-21 12:11:45 -0500

    fix: use the right settings for each environment

--- tutorcodejail/templates/codejail/apps/config/tutor.py
+++ tutorcodejail/templates/codejail/apps/config/tutor.py
@@ -1,16 +1,16 @@
 """Module with the configuration of config classes."""
-from codejailservice.config import BaseConfig
+from codejailservice.config import DevelopmentConfig, ProductionConfig
 
 
-class DevelopmentConfig(BaseConfig):
-    """Class to use for development context that inherits from BaseConfig."""
+class DevelopmentConfig(DevelopmentConfig):
+    """Class to use for development context that inherits from DevelopmentConfig."""
 
     {{patch("codejail-common-settings")}}
     {{patch("codejail-development-settings")}}
 
 
-class ProductionConfig(BaseConfig):
-    """Class to use for production context that inherits from BaseConfig."""
+class ProductionConfig(ProductionConfig):
+    """Class to use for production context that inherits from ProductionConfig."""
 
     {{patch("codejail-common-settings")}}
     {{patch("codejail-production-settings")}}
