5c33ccc922a0c488ec142167b2f258e4687af3d9
Maria Fernanda Magallanes Zubillaga <maria.magallanes@edunext.co>
2024-05-22 17:27:06 -0500

    fix: add code_jail to be able to edit it trough patches

--- tutorcodejail/templates/codejail/apps/config/tutor.py
+++ tutorcodejail/templates/codejail/apps/config/tutor.py
@@ -5,6 +5,7 @@ from codejailservice.config import DevelopmentConfig, ProductionConfig
 class DevelopmentConfig(DevelopmentConfig):
     """Class to use for development context that inherits from DevelopmentConfig."""
 
+    CODE_JAIL = DevelopmentConfig.CODE_JAIL
     {{patch("codejail-common-settings")}}
     {{patch("codejail-development-settings")}}
 
@@ -12,5 +13,6 @@ class DevelopmentConfig(DevelopmentConfig):
 class ProductionConfig(ProductionConfig):
     """Class to use for production context that inherits from ProductionConfig."""
 
+    CODE_JAIL = ProductionConfig.CODE_JAIL
     {{patch("codejail-common-settings")}}
     {{patch("codejail-production-settings")}}
