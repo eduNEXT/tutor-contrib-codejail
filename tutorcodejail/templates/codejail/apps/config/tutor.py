794acfab2e70d688034867c9bfd967d3fb09c0db
Maria Fernanda Magallanes Zubillaga <maria.magallanes@edunext.co>
2024-05-23 10:50:24 -0500

    fix: add indent to the patches

--- tutorcodejail/templates/codejail/apps/config/tutor.py
+++ tutorcodejail/templates/codejail/apps/config/tutor.py
@@ -6,13 +6,13 @@ class DevelopmentConfig(DevelopmentConfig):
     """Class to use for development context that inherits from DevelopmentConfig."""
 
     CODE_JAIL = DevelopmentConfig.CODE_JAIL
-    {{patch("codejail-common-settings")}}
-    {{patch("codejail-development-settings")}}
+    {{patch("codejail-common-settings") | indent(4)}}
+    {{patch("codejail-development-settings") | indent(4)}}
 
 
 class ProductionConfig(ProductionConfig):
     """Class to use for production context that inherits from ProductionConfig."""
 
     CODE_JAIL = ProductionConfig.CODE_JAIL
-    {{patch("codejail-common-settings")}}
-    {{patch("codejail-production-settings")}}
+    {{patch("codejail-common-settings") | indent(4)}}
+    {{patch("codejail-production-settings") | indent(4)}}
