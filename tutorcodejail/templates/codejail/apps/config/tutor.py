03111d12dad4230f035730dd6d7340a5bc55e4d4
María Fernanda Magallanes <35668326+MaferMazu@users.noreply.github.com>
2024-05-24 10:07:46 -0500

    fix: use tutor config on codejail service and add patches to edit it DS-894 (#55)
    * fix: use tutor config on codejail service and add patches to edit it
    
    * fix: use the right settings for each environment
    
    * fix: add code_jail to be able to edit it trough patches
    
    * fix: add indent to the patches

--- tutorcodejail/templates/codejail/apps/config/tutor.py
+++ tutorcodejail/templates/codejail/apps/config/tutor.py
@@ -1,10 +1,18 @@
 """Module with the configuration of config classes."""
-from base import BaseConfig
+from codejailservice.config import DevelopmentConfig, ProductionConfig
 
 
-class DevConfig(BaseConfig):
-    """Class to use for development context that inherits from BaseConfig."""
+class DevelopmentConfig(DevelopmentConfig):
+    """Class to use for development context that inherits from DevelopmentConfig."""
+
+    CODE_JAIL = DevelopmentConfig.CODE_JAIL
+    {{patch("codejail-common-settings") | indent(4)}}
+    {{patch("codejail-development-settings") | indent(4)}}
 
 
-class ProductionConfig(BaseConfig):
-    """Class to use for production context that inherits from BaseConfig."""
+class ProductionConfig(ProductionConfig):
+    """Class to use for production context that inherits from ProductionConfig."""
+
+    CODE_JAIL = ProductionConfig.CODE_JAIL
+    {{patch("codejail-common-settings") | indent(4)}}
+    {{patch("codejail-production-settings") | indent(4)}}
